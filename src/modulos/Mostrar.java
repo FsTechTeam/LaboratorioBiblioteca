dda4913d56c934b2b5f12f0699f889a519f8b8f1
Lester Meneses <lesterjosue1@gmail.com>
2015-06-23 11:25:27 -0600

    IMPLEMENTADO OBSERVER Y OBTENERDATOS()

---
+++ src/modulos/Mostrar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modulos;
+
+import java.util.List;
+
+/**
+ *
+ * @author Geek
+ */
+public interface Mostrar {
+    
+    public List<Object> obtenerDatos(); 
+    
+}
